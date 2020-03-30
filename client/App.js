00845015668c6a490bce7455ab26dbfa4fed0da8
Saeed Sheikh <saeedshaikh166@gmail.com>
2020-03-29 22:50:29 -0400

    Commits latest changes to the client

--- client/App.js
+++ client/App.js
@@ -1,19 +1,75 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Button,
+  Text,
+  TextInput,
+  SafeAreaView,
+  View
+} from "react-native";
+import { apiUrl } from "./config";
 
-export default function App() {
+const App = () => {
+  const [score, setScore] = useState(0);
+  const [tag, setTag] = useState("");
+
+  const getPrediction = async () => {
+    try {
+      console.log(`${apiUrl}"${tag}"`);
+      const response = await fetch(`${apiUrl}"${tag}`, {
+        method: "GET",
+        mode: "cors"
+      });
+
+      const retrievedScore = await response;
+      console.log(retrievedScore);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <SafeAreaView>
+        <Text style={styles.header}>How do Twitter users feel?</Text>
+        <Text style={styles.prompt}>
+          Enter a hashtag or something trending below, to find out
+        </Text>
+        <TextInput
+          style={styles.textIn}
+          onChangeText={text => setTag(text)}
+          value={tag}
+        />
+        <Button
+          onPress={getPrediction}
+          title="Predict"
+          color="#4B88A2"
+          accessibilityLabel="Get's prediction of how people feel"
+        />
+      </SafeAreaView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    backgroundColor: "#CDE7B0",
+    alignItems: "center",
+    justifyContent: "space-between"
   },
+  header: {
+    fontSize: 30
+  },
+  prompt: {
+    fontSize: 14
+  },
+  textIn: {
+    height: 30,
+    borderColor: "gray",
+    borderWidth: 1
+  }
 });
+
+export default App;
